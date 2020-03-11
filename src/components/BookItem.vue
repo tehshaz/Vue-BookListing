a1cdc5998bba37dd697771861b0b13be10f80290
tehshaz <tehshaz@gmail.com>
2020-03-11 16:14:12 -0500

    Completed module

---
+++ src/components/BookItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ol>
+      <li>{{ book.title }}:{{ book.author }}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BookItem',
+    props: [
+      'book',
+    ],
+  }
+</script>
+
+<style>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
