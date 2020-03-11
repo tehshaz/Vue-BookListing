ab12c92b147260f4374a9db86c5ed18588ccc1ab
tehshaz <tehshaz@gmail.com>
2020-03-11 16:01:09 -0500

    Module1

---
+++ src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: 'BookList',
+        title: 'All Books'
+      }
+    },
+  }
+</script>
+
+<style>
+h1,h2 {
+  font-weight: normal;
+}
+</style>
